5ff7a29b717fa7c095331387c4fe54458a95e95c
Alejandra <alejandrafernandezg9701@gmail.com>
2022-04-13 17:16:30 -0500

    Creacion de componentes y modulos necesarios

---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AgregarComponent } from './pages/agregar/agregar.component';
+import { BuscarComponent } from './pages/buscar/buscar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ListadoComponent } from './pages/listado/listado.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
+
+
+
+@NgModule({
+  declarations: [
+    AgregarComponent,
+    BuscarComponent,
+    HomeComponent,
+    ListadoComponent,
+    HeroeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HeroesModule { }
